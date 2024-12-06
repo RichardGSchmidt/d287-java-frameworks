fcad45963d52085fbf0ab54b41bdc88d8eabbb27
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2024-12-06 10:57:40 -0600

    > Task H. Filename ValidEnufParts.java Changes/Lines: Updated the error message to better reflect whats happening (line 20).

--- src/main/java/com/example/demo/validators/ValidEnufParts.java
+++ src/main/java/com/example/demo/validators/ValidEnufParts.java
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnufParts {
-    String message() default "There aren't enough parts in inventory!";
+    String message() default "Unable to add, this would lower levels below the minimum inventory values!";
     Class<?> [] groups() default {};
     Class<? extends Payload> [] payload() default {};
 
