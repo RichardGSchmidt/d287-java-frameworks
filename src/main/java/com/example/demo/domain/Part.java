f4fb12690ab3ab69eee0d450ce2755285a049296
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2024-12-06 10:07:26 -0600

    Task G. Filename: Part.Java, Lines/Changes: Added minInv and maxInv fields (lines 32-35), an updated constructor using these values (lines 59-67), and set and get functions for each field (lines 74-84).

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -28,6 +28,12 @@ public abstract class Part implements Serializable {
     double price;
     @Min(value = 0, message = "Inventory value must be positive")
     int inv;
+    //declaration of new minimum and maximum inventory values in accordance with Task G
+    @Min(value = 0, message = "Minimum inventory value must be positive")
+    int minInv = 0;
+    @Min(value = 0, message = "Maximum value must be positive")
+    int maxInv = Integer.MAX_VALUE;
+
 
     @ManyToMany
     @JoinTable(name="product_part", joinColumns = @JoinColumn(name="part_id"),
@@ -50,9 +56,33 @@ public abstract class Part implements Serializable {
         this.inv = inv;
     }
 
+    //new constructor that includes min and max inv
+    public Part(long id, String name, double price, int inv, int minInv, int maxInv) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.inv = inv;
+        this.minInv = minInv;
+        this.maxInv = maxInv;
+    }
+
+
     public long getId() {
         return id;
     }
+
+    //get and set functions for min and max inventory in accordance with Task G
+    public int getMinInv() { return minInv; }
+    public void setMinInv(int minInv) { this.minInv = minInv; }
+
+    public int getMaxInv() { return maxInv; }
+    public void setMaxInv(int maxInv) { this.maxInv = maxInv; }
+
+    //inventory validation function checks to ensure that inventory is between min and max values.
+    public boolean isInventoryValid(){ return ((minInv <= inv) && (inv <= maxInv)); }
+    //further check to ensure that min value is less than or equal to max value
+    public boolean isMaxInvValid(){ return (maxInv >= minInv); }
+
 
     public void setId(long id) {
         this.id = id;
